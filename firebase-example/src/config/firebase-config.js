9332b0ffff66cfb76b9689896403816219a04e25
saif-at-scalekit <saif.shaik@scalekit.com>
2025-06-25 12:23:29 +0530

    move from separate repo to this (#2)

---
+++ firebase-example/src/config/firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC86AOMsSLM3GJeFp1UP-CSD0jLmwa7vzw",
+  authDomain: "devramp-27fb1.firebaseapp.com",
+  projectId: "devramp-27fb1",
+  storageBucket: "devramp-27fb1.appspot.com",
+  messagingSenderId: "373840568539",
+  appId: "1:373840568539:web:f07955d10e526682266cfe",
+  measurementId: "G-Z40QT75ZGX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
